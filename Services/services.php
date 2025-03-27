5826d1c4f7b9614468e6d6fc2075ec770930c3fa
Stephen Robles <robles_s@f213.ub.lan>
2025-03-27 18:02:37 +0800

    Services

---
+++ Services/services.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        
+    </style>
+</head>
+<body>
+    <section class="Jumbotron text-center">
+    <div class="container">
+        <div class="row">
+            <h1 class="jumbotron-heading">Services Offered</h1>
+        </div>
+    </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <h4>Service</h4>
+                        <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare neque ut justo vehicula pharetra. Quisque at rutrum nisl. Suspendisse ullamcorper tellus purus, eget rhoncus eros vehicula id. Morbi sed est elit
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Set Appointment</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
